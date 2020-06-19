feb6b14565c63c191e5e13bfed744431ccf77de3
jukomol <komolma37@gmail.com>
2020-06-19 17:55:19 +0600

    clear button added
    Now you can clear link and you can download again without quitting.

--- yt downloader/ytdownloader.py
+++ yt downloader/ytdownloader.py
@@ -9,11 +9,13 @@ window.resizable(0,0)
 window.title("YT DOWNLOADER")
 window['background']='#8c8c8c'
 
-progressbar=ttk.Progressbar(window, orient=HORIZONTAL,length=180,mode='determinate')
+progressbar=ttk.Progressbar(window, orient=HORIZONTAL,length=200,mode='determinate')
 
 def step():
+    global l1
     progressbar['value']=100
-    Label(window, text="Downloaded",width=12).grid(row=5,column=1,padx=5,pady=5)
+    l1=Label(window, text="Downloaded",width=12)
+    l1.grid(row=5,column=1,padx=5,pady=5)
     #progressbar.start(5)
     '''
     for i in range(5):
@@ -21,6 +23,11 @@ def step():
         window.update_idletasks()
         time.sleep(1)
             '''
+
+def clearText():
+    title_entry.delete(0,'end')
+    progressbar.stop()
+    l1.destroy()
 
 
 def ytdownload():
@@ -47,7 +54,9 @@ path_entry.grid(row=1, column=1,padx=5,pady=5,columnspan=3)
 b1=Button(window,text="Download", width=14,command=ytdownload)
 b1.grid(row=3,column=1,pady=5)
 
-progressbar.grid(row=4,column=0,pady=5,columnspan=4)
+b2=Button(window,text="Clear", width=14,command=clearText)
+b2.grid(row=3,column=2,pady=5,padx=5)
 
+progressbar.grid(row=4,column=1,pady=5,columnspan=4)
 
 window.mainloop()
